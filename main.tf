2269e96a0c3667320a35b81e21efc2b2101565af
HanumanthRM <hanu_mum@hotmail.com>
2022-01-15 13:53:57 +0530

    create third RG

--- main.tf
+++ main.tf
@@ -23,3 +23,9 @@ resource "azurerm_resource_group" "example1" {
   name     = "hanumanthiac7_1"
   location = "West Europe"
 }
+
+# Create a resource group
+resource "azurerm_resource_group" "example3" {
+  name     = "hanumanthiac7_it-rg"
+  location = "West Europe"
+}
